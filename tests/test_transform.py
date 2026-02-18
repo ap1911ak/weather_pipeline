ee4a34d7cf901d8f772c17432227455ae80b21c2
CrocodileSoy <ap1911ak@gmail.com>
2026-02-18 17:27:47 +0700

    add test_transform.py for test transform.py

---
+++ tests/test_transform.py
@@ -0,0 +1,15 @@
+import pytest
+from src.transform import clean_weather_data
+
+def test_clean_weather_data_logic():
+    # สร้างข้อมูลจำลอง (Mock Data)
+    mock_input = {
+        "hourly": {
+            "time": ["2023-10-01T00:00", "2023-10-01T01:00"],
+            "temperature_2m": [25.5, 32.0]
+        }
+    }
+    df = clean_weather_data(mock_input)
+    
+    assert len(df) == 2
+    assert df.loc[1, 'is_hot'] == True  # 32.0 > 30 ต้องเป็น True
